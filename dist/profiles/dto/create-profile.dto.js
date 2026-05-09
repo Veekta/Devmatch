a9a1b50ce697d924556dcf5097068272a6675e69
Veekta <vsunday.1710@gmail.com>
2026-05-09 09:18:46 -0600

    remove dist from tracking and update gitignore

--- dist/profiles/dto/create-profile.dto.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class createProfileDto {
-}
-exports.default = createProfileDto;
-//# sourceMappingURL=create-profile.dto.js.map
